3286eadb1a46d447fc23342588cb7a443d188df3
nschampions2004 <kylerligon@gmail.com>
2021-06-15 22:21:14 -0500

    first attempt

--- 20210615/20210615.R
+++ 20210615/20210615.R
@@ -8,4 +8,119 @@ plots_folder <- "plots"
 predictions_folder <- "predictions"
 models_folder <- "models"
 
-theme_set(theme_minimal)
+theme_set(theme_minimal())
+
+
+income_data <- readxl::read_xls(file.path(data_folder, "tabn025.xls")) %>% 
+  janitor::clean_names() %>% 
+  tidyr::separate(col = state, sep = " \\.",
+                  into = c("state", "trash")) %>% 
+  filter(state != "District") %>% 
+  select(state, median_household_income = x2010)
+
+store_training <- read_csv(file.path(data_folder, "train.csv")) %>% 
+  left_join(income_data, by = "state") %>% 
+  mutate_if(is.character, as.factor) 
+store_test <- read_csv(file.path(data_folder, "test.csv")) %>% 
+  left_join(income_data, by = "state") %>% 
+  mutate_if(is.character, as.factor)
+
+# Alright, with a good amount of EDA out of the way,
+# 1. Speed run an XGboost model with _only_the numeric variables 
+#     on 5 fold cv.... submit as my baseline.
+# 2. Bring in the categorical variables accordingly 
+#     hoping to bring rmse down
+# 3. Incorporate more data????
+#   - Census Data (Populations)
+#   - median househole income per state?
+
+
+# Thoughts... 
+# I'm starting to see why we should be pulling more data in.
+# I'm seeing a whole subsection of states missing
+# I've got a feeling that "Region" field won't be able to impart the necessary information
+# that we'd need to do well on the testing set
+# 
+
+
+store_recipe <- recipe(profit ~ ., store_training) %>% 
+  update_role(id, new_role = "id") %>% 
+  step_rm(id, 
+          postal_code,
+          country) %>% 
+  step_other(city, threshold = 0.01) %>% 
+  step_dummy(segment,
+           city,
+           state,
+           region, 
+           category, 
+           sub_category) %>% 
+  prep()
+
+
+training <- bake(store_recipe, new_data =NULL )
+testing  <- bake(store_recipe, new_data =store_test)
+
+set.seed(42069) # such a hard decision
+stores_folds <- vfold_cv(data = training, v = 5)
+
+stores_xgb <- boost_tree(mode = "regression",
+                         learn_rate = tune(),
+                         trees = 500,
+                         mtry = tune(),
+                         tree_depth = tune()
+                         ) %>% 
+  set_engine("xgboost")
+
+stores_wkflow <- workflow() %>% 
+  add_model(stores_xgb) %>% 
+  add_formula(profit ~ .)
+
+stores_metrics <- metric_set(rmse)
+
+doParallel::registerDoParallel(5)
+stores_grid <- tune_grid(stores_wkflow,
+   resamples = stores_folds,
+   grid = crossing(learn_rate = c(0.3, 0.4),
+                   mtry = c(0.8, 1.0),
+                   tree_depth = c(3, 5)),
+   metrics = stores_metrics,
+   control = control_grid(verbose = TRUE, save_pred = TRUE))
+
+# $10 all models fail
+
+# check out the behavior
+stores_grid %>% autoplot()
+
+stores_grid %>% 
+  collect_metrics() %>% 
+  ggplot(aes(x = learn_rate, y = mean)) +
+  geom_point() +
+  labs(title = "initial training results
+# higher mtry and learn rate the better with 500 trees... which is expected with 4 variables
+")
+
+# 78 for 4
+# 88 for 6
+# 88 for 7
+
+# fit the
+best_params <- stores_grid %>% 
+  select_best()
+
+final_stores_xgb <- stores_xgb %>% 
+  finalize_model(best_params)
+
+final_fit <- final_stores_xgb %>% 
+  fit(formula = profit ~ ., data = training)
+
+final_preds <- final_fit %>% 
+  predict(testing) %>% 
+  bind_cols(store_test) %>% 
+  select(id, profit = .pred)
+
+final_preds %>% 
+  write_csv(file.path(predictions_folder, "attempt7_more_params_more_tune_even_more_tune.csv"))
+
+
+
---
+++ 20210615/utils.R
@@ -0,0 +1,158 @@
+library(tidyverse)
+library(tidymodels)
+library(DataExplorer)
+library(skimr)
+library(ggridges)
+
+# EVERY DFKDSFD:SKF TIME !!!!!
+theme_set(theme_minimal())
+
+data_folder <- 'data'
+preds_folder <- 'preds'
+plots_folder <- 'plots'
+models_folder <- 'models'
+
+
+clean_feat_matrix <- function(df_before_dmatrix, response_variable) {
+  #' @param: df_before_dmatrix {tibble} the tibble before it's turned into a dmatrix
+  #' @param: response_variable {string} the response variable to be predicted
+  #'
+
+  if (!is.null(response_variable)) {
+    response_name <- as.name(response_variable)
+
+    long_feat_df <- df_before_dmatrix %>%
+      mutate(id = row_number()) %>%
+      pivot_longer(names_to = "features",
+                   values_to = "feature_value",
+                   cols = -c(id, all_of(response_name)))
+  } else {
+    long_feat_df <- df_before_dmatrix %>%
+      mutate(id = row_number()) %>%
+      pivot_longer(names_to = "features",
+                   values_to = "feature_value",
+                   cols = -c(id))
+  }
+
+  long_feat_df <-  long_feat_df %>%
+    filter(feature_value != 0) %>%
+    mutate(features = str_replace_all(features, pattern = "\\.+", replacement = '_')) %>%
+    separate(features,
+             into = c("feature", "subfeature"),
+             remove = FALSE,
+             extra = "merge")
+
+  long_feat_df
+}
+
+clean_shaps_matrix <- function(xgb_preds) {
+  #' @param: xgb_preds {tibble} the tibble after predicting SHAPs from tidy models shap
+  #'
+
+  shaps_df <- as.data.frame(xgb_preds) %>%
+    as_tibble() %>%
+    select(-BIAS) %>%
+    mutate(id = row_number()) %>%
+    pivot_longer(names_to = "subfeatures", values_to = "shaps",
+                 cols = -id) %>%
+    mutate(subfeatures = str_replace_all(subfeatures,
+                                         pattern = "\\.+",
+                                         replacement = '_')) %>%
+    separate(subfeatures,
+             into = c("feature", "subfeature"),
+             remove = FALSE,
+             extra = "merge") %>%
+    group_by(id, feature) %>%
+    summarize(shaps = sum(shaps), .groups = "drop")
+}
+
+combine_shaps_and_features <- function(shaps_frame, feat_df) {
+  #' @param: shap_frame {tibble}: the tibble from `clean_shaps_matrix`
+  #' @param: feat_df {tibble}: the tibble from `clean_feat_matrix`
+
+  shaps_to_graph <- shaps_frame %>%
+    inner_join(feat_df, by = c("id", "feature"))
+
+  cat_feats <- shaps_to_graph %>%
+    filter(!is.na(subfeature))
+
+  num_feats <- shaps_to_graph %>%
+    filter(is.na(subfeature))
+
+  return_vec <- c()
+  return_vec[['cats']] <- cat_feats
+  return_vec[['numeric']] <- num_feats
+
+  return_vec
+}
+
+shapper_cats <- function(x = NULL, df, sample_size = NULL) {
+  #' @param: x {string}: the categorical variable to plot grouped densities from
+  #' @param: df {tibble}: the categorical data frame output from `combine_shaps_and_features`
+  #' @param: sample_size {int}: how many shap value to sample from the total
+  #'
+
+  if (!is.null(sample_size)) {
+    set.seed(69420)
+    df <- df %>%
+      sample_n(sample_size)
+  }
+
+  if (!is.null(x)) {
+    df <- df %>%
+      filter(str_detect(string = feature, pattern = x)) %>%
+      mutate(subfeature = fct_reorder(subfeature, shaps, .fun = median))
+
+    var_for_title <- unique(df$feature)[1]
+  } else {
+    df <- df %>%
+      mutate(subfeature = fct_reorder(subfeature, shaps, .fun = median))
+
+    var_for_title <- "All Categorical Variables"
+  }
+
+  df %>%
+    ggplot(aes(x = shaps, y = subfeature, alpha = 0.7)) +
+    ggridges::geom_density_ridges() +
+    stat_summary(fun = "median", geom = "errorbar", aes(xmax = ..x.., xmin = ..x..), linetype = 2) +
+    labs(x = NULL,
+         y = NULL,
+         title = glue::glue("SHAP Values for {var_for_title}"),
+         subtitle = 'sorted by median SHAP value, within group median listed with dotted line') +
+    theme(legend.position = "none",
+          plot.title.position = "plot")
+}
+
+shapper_numeric <- function(x = NULL, df, sample_size = NULL) {
+  #' @param: x {string}: the numerical variable to plot densities from
+  #' @param: df {tibble}: the numeric data frame output from `combine_shaps_and_features`
+  #' @param: sample_size {int}: how many shap value to sample from the total
+  #'
+
+
+  if (!is.null(sample_size)) {
+    set.seed(69420)
+    df <- df %>%
+      sample_n(sample_size)
+  }
+
+  if (!is.null(x)) {
+    df <- df %>%
+      filter(str_detect(string = feature, pattern = x))
+
+    var_for_title <- unique(df$feature)[1]
+  } else {
+    var_for_title <- "All Numeric Variables"
+  }
+
+  df %>%
+    ggplot(aes(x = feature_value, y = shaps)) +
+    geom_point() +
+    geom_smooth() +
+    facet_wrap(~feature, scales = "free_x") +
+    labs(x = NULL,
+         y = NULL,
+         title = glue::glue("SHAP Values for {var_for_title}")) +
+    theme(legend.position = "none",
+          plot.title.position = "plot")
+}
